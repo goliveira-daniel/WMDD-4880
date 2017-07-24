6041b796d6303f055fa355b5ca25a1c1950ad1e2
goliveira-daniel <goliveira.daniel@gmail.com>
2017-07-23 22:00:15 -0700

    Added tags filter and skeleton boilerplate

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var feed = new Instafeed({
 	get: 'user',
 	// get: 'tagged',
 	// tagName: 'wmdd4880',
-	userId: 5219349,
+	userId: 52193489,
 	accessToken: '229774133.ee26814.d9279c18526f4a578198a7501a4f98e3',
 	target: 'instagram',
 	resolution: 'standard_resolution',
@@ -13,12 +13,29 @@ var feed = new Instafeed({
 
 window.onload = function() {
 	$("button").on("click",function() {
-		let txt = document.getElementById("rule")
-		feed.options.userId = parseInt(txt.value)
-		feed.run();
+		$('#participants').empty();
+		// myTags.options.target = 'participants';
+		// feed.run();
+		// get multiple tags
+		let myTags = getMultipleTags($.map($('input').val().split(','), $.trim));
+		// run each instance
+		for(var i=0, len=myTags.length; i < len; i++) {
+			myTags[i].run();
+		}
 	})
+}
 
-	feed.options.target = 'participants';
-	feed.options.template = "<div id='box'><div id='picture'><img src='{{image}}'><span id='center-heart' class='icon-heart'></span></div><div id='infos'><span id='icon-instagram' class='icon-instagram'></span><div id='user'>{{model.user.username}}</div><div id='bottom-heart' class='icon-heart unclicked'></div></div></div>";
-	feed.run();
+function getMultipleTags (tags) {
+	var feeds = [];
+    for (var i=0, len=tags.length; i < len; i++) {
+        feeds.push(new Instafeed({
+			accessToken: '229774133.ee26814.d9279c18526f4a578198a7501a4f98e3',
+			template: "<div id='box'><div id='picture'><img src='{{image}}'><span id='center-heart' class='icon-heart'></span></div><div id='infos'><span id='icon-instagram' class='icon-instagram'></span><div id='user'>{{model.user.username}}</div><div id='bottom-heart' class='icon-heart unclicked'></div></div></div>",
+			target: 'participants',
+            get: 'tagged',
+            tagName: tags[i],
+            // target: "instafeed-" + tags[i]
+        }));
+    }
+    return feeds;
 }
